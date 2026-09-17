830ccb024a581187997c5ca4e72aca4498940dee
Nishant Yadav <113028102+nishanty23@users.noreply.github.com>
2026-09-17 23:37:54 +0530

    Modify output format in HalfPyramidNumbers

--- Java/Pattern/HalfPyramidNumbers.java
+++ Java/Pattern/HalfPyramidNumbers.java
@@ -7,7 +7,7 @@ class HalfPyramidNumbers{
         int row = sc.nextInt();
         for(int i=1; i<=row; i++){
             for(int j=1; j<i+1; j++){
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.println();
         }
