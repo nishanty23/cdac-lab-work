9dea27f08bf0c37736b824640ebc4143e71f8a90
Nishant Yadav <113028102+nishanty23@users.noreply.github.com>
2026-09-18 20:53:53 +0530

    Refactor Inverted Half Pyramid number printing logic

--- Java/Pattern/InvertedHalfPyramidNumbers.java
+++ Java/Pattern/InvertedHalfPyramidNumbers.java
@@ -5,18 +5,25 @@ class InvertedHalfPyramidNumbers{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int row = sc.nextInt();
-        int i = row, j = 0, k = 1, x = 0;
-        while(i > 0){
-            j = i;
-            k = 1;
-            x = 1;
-            while(j > 0){
-                System.out.print(x + " ");
-                x++;
-                j--;
+        //int i = row, j = 0, k = 1, x = 0;
+        //while(i > 0){
+        //    j = i;
+        //    k = 1;
+        //    x = 1;
+        //    while(j > 0){
+        //        System.out.print(x + " ");
+        //        x++;
+        //        j--;
+        //    }
+        //    System.out.println();
+        //    i--;
+        //}
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<= row-i+1; j++){
+                System.out.print(j);
             }
             System.out.println();
-            i--;
         }
     }
 }
