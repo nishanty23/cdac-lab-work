3d8008226ecb8251513f1f650b963fa0214aaff9
Nishant Yadav <113028102+nishanty23@users.noreply.github.com>
2026-09-18 20:46:20 +0530

    Rename class to InvertedHalfPyramidNumbersReverse

--- Java/Pattern/InvertedHalfPyramidNumbersReverse.java
+++ Java/Pattern/InvertedHalfPyramidNumbersReverse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class InvertedHalfPyramidNumbers{
+class InvertedHalfPyramidNumbersReverse{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
